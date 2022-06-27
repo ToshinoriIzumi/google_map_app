6c5b79c992d3231103142f2ea519e47aae350715
izumi.toshinori <toshinori622@gmail.com>
2022-06-27 21:29:58 +0900

    JSの修正

--- app/assets/javascripts/google_map_test.js
+++ app/assets/javascripts/google_map_test.js
@@ -1,8 +1,8 @@
-import { GOOGLE_MAP_API_KEY } from 'config/const.js'
+import GOOGLE_MAP_API_KEY  from 'config/const.js'
 
-console.log(GOOGLE_MAP_API_KEY());
+console.log(GOOGLE_MAP_API_KEY);
 const script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAP_API_KEY() + '&callback=initMap';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAP_API_KEY + '&callback=initMap';
 window.initMap = function() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
